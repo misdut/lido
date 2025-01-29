703d45dbed79d9ab749621b436a37fad5d18865c
misdut <daniellybarbosa760@gmail.com>
2025-01-29 01:23:50 -0300

    Add the Show func

--- lido/lido.go
+++ lido/lido.go
@@ -1,21 +1,51 @@
 package lido
 
-type Todo struct {
-	Title	string
-	Date	string
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
+var currentTime = time.Now().Local().Format("02/01/2006")
+
+type Todo struct {
+	Title string
+	Date  string
 }
 
 type Todos struct {
-	List	[]Todo
+	List []Todo
 }
 
-func (t *Todos) Add(title string, date string) {
-	t.List = append(t.List, Todo{Title: title, Date: date})
+func (t *Todos) Add(title string) {
+	t.List = append(t.List, Todo{Title: title, Date: currentTime})
+}
+
+func (t Todos) Show() string {
+	s := ""
+	for i := 0; i < len(t.List); i++ {
+		s += strconv.Itoa(i+1) + " " + t.List[i].Title + " - " + t.List[i].Date + "\n"
+	}
+
+	return s
+}
+
+func (t *Todos) Del(index string) {
+	i, _ := strconv.Atoi(index)
+
+	if i < 1 || i-1 >= len(t.List) {
+		fmt.Println("The given index is out of bounds.")
+	} else {
+		currentList := append(t.List[:(i-1)], t.List[(i-1)+1:]...)
+		t.List = currentList
+
+		fmt.Println(t.Show())
+
+	}
 }
 
 var TodoList Todos = Todos{List: []Todo{
-	{Title: "Something to do", Date: "20-01-2025"},
-	{Title: "Somewhere to go", Date: "25-01-2025"},
-	{Title: "Something to buy", Date: "26-01-2023"},
+	{Title: "Something to do", Date: currentTime},
+	{Title: "Somewhere to go", Date: currentTime},
+	{Title: "Something to buy", Date: currentTime},
 }}
