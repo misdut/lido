1ef54f586833847d3f9da4fbbdbfa8ce8c02cc8b
misdut <daniellybarbosa760@gmail.com>
2025-01-29 01:27:54 -0300

    Add changes Cmd package

--- cmd/add.go
+++ cmd/add.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+
 	"todo/lido"
 
 	"github.com/spf13/cobra"
@@ -11,11 +12,11 @@ var addCmd = &cobra.Command{
 	Use:     "add",
 	Aliases: []string{"addition"},
 	Short:   "Add the task to the list",
-	Long:    "Add title and date to task list",
-	Args:    cobra.ExactArgs(2),
+	Long:    "Add your task to the list",
+	Args:	 cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		lido.TodoList.Add(args[0], args[1])
-		fmt.Println(lido.TodoList)
+		lido.TodoList.Add(args[0])
+		fmt.Printf("Task \"%v\" added successfully.\n", args[0])
 
 	},
 }
