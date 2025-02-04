1c6542dd4dea6b586794d8f375be04b6cac55fa8
misdut <daniellybarbosa760@gmail.com>
2025-02-04 00:28:15 -0300

    Update toggle.go

--- cmd/toggle.go
+++ cmd/toggle.go
@@ -10,7 +10,6 @@ var toggleCmd = &cobra.Command{
 	Use:     "t",
 	Aliases: []string{"toggle"},
 	Short:   "Toggle checks task as done",
-	Args:	 cobra.ExactArgs(1),
 	Run:	func(cmd *cobra.Command, args []string) {
 		todo.TodoList.ToggleChecked(args[0])
 	},
